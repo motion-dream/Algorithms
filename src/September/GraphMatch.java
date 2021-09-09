92a98135b3196f216033175d5c77e7541c729b39
2867405686@qq.com <2867405686@qq.com>
2021-09-09 11:05:55 +0800

    update Assignment problem

---
+++ src/September/GraphMatch.java
@@ -0,0 +1,19 @@
+package September;
+
+/**
+ * @author jmm
+ * @version 1.0
+ * @date 2021/9/8 19:42
+ * @email 2867405686@qq.com
+ */
+public class GraphMatch {
+
+    public static void main(String[] args) {
+        Integer[][] values = {{3,8,2,10},{9,7,5,3},{1,5,4,3},{4,5,7,9}};
+
+        App hungarian1=new App(values);
+    }
+
+}
+
+
